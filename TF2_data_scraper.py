7d2fb58fd9f3324206397d1956697538bfb516d6
Seth Grimes <seth.grimes1999@gmail.com>
2024-10-30 16:28:26 -0400

    Added ammo

--- TF2_data_scraper.py
+++ TF2_data_scraper.py
@@ -25,7 +25,8 @@ def create_table(conn):
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             class TEXT NOT NULL,
-            slot TEXT NOT NULL
+            slot TEXT NOT NULL,
+            ammo TEXT NOT NULL
         );
         '''
         cursor.execute(sql_create_table)
@@ -37,8 +38,8 @@ def create_table(conn):
 def insert_weapon(conn, weapon):
     try:
         sql_insert_weapon = '''
-        INSERT INTO weapons (name, class, slot)
-        VALUES (?, ?, ?);
+        INSERT INTO weapons (name, class, slot, ammo)
+        VALUES (?, ?, ?, ?);
         '''
         cursor = conn.cursor()
         cursor.execute(sql_insert_weapon, weapon)
@@ -54,6 +55,9 @@ def scrape_data(conn, url):
 
     current_class = None
     current_slot = None
+    ammo = None
+    loaded_ammo = None
+    carried_ammo = None
 
     # Loop over each element in the page
     for header in soup.find_all(['h2', 'h3', 'table']):
@@ -89,15 +93,17 @@ def scrape_data(conn, url):
                             while temp_string.isalpha() == False:
                                 temp_string = current_slot[0:len(temp_string)-1]
                             current_slot = temp_string
+                            ammo = "N/A"
+                                
                             # Insert weapon data into the database
-                            insert_weapon(conn, (weapon_name, current_class, current_slot))
+                            insert_weapon(conn, (weapon_name, current_class, current_slot, ammo))
             else:
                 # For other classes and slots, skip the first two rows
                 for row in header.find_all('tr')[2:]:  # Skip first two rows
                     hasAlreadyInsertedPDAs = False
                     if "PDA" in current_slot and current_class == "Engineer" and hasAlreadyInsertedPDAs == False:
-                        insert_weapon(conn, ("Construction PDA", current_class, "PDA"))
-                        insert_weapon(conn, ("Destruction PDA", current_class, "PDA"))
+                        insert_weapon(conn, ("Construction PDA", current_class, "PDA", "N/A"))
+                        insert_weapon(conn, ("Destruction PDA", current_class, "PDA", "N/A"))
                         hasAlreadyInsertedPDAs = True
                         continue
                     weapon_cell = row.find('th')
@@ -115,8 +121,60 @@ def scrape_data(conn, url):
                             while temp_string.isalpha() == False:
                                 temp_string = current_slot[0:len(temp_string)-1]
                             current_slot = temp_string
+                            # Find all <td> elements after the weapon name
+                            td_elements = row.find_all('td')
+                            
+                            if current_slot == "Melee":
+                                ammo = "N/A"
+                            elif (current_class == "Heavy" and current_slot == "Primary") or (weapon_name == "Widowmaker" or weapon_name == "Short Circuit"):
+                                ammo = 200
+                            elif (current_class == "Pyro" and current_slot != "Melee"):
+                                if weapon_name == "Flare Gun" or weapon_name == "Detonator" or weapon_name == "Scorch Shot":
+                                    ammo = "1, 16"
+                                elif weapon_name != "Dragon's Fury" and current_slot == "Primary":
+                                    ammo = "200, N/A"
+                                elif weapon_name == "Dragon's Fury":
+                                    ammo = "40, N/A"
+                                elif weapon_name == "Thermal Thruster":
+                                    ammo = "2, N/A"
+                                elif weapon_name == "Gas Passer" or weapon_name == "Manmelter":
+                                    ammo = "1, ∞"
+                            elif weapon_name == "Ali Baba's Wee Booties" or weapon_name == "Bootlegger" or weapon_name == "Giger Counter" or weapon_name == "Sandvich" or weapon_name == "Robo-Sandvich" or weapon_name == "Dalokohs Bar" or weapon_name == "Fishcake" or weapon_name == "Razorback":
+                                ammo = "N/A"
+                            elif weapon_name == "Crusader's Crossbow":
+                                ammo = "1, 38"
+                            elif current_class == "Sniper" and current_slot != "Melee":
+                                if weapon_name == "Huntsman" or weapon_name == "Fortified Compound":
+                                    ammo = "1, 12"
+                                elif weapon_name == "Jarate" or weapon_name == "Self-Aware Beauty Mark" or weapon_name == "Razorback":
+                                    ammo = "1, ∞"
+                                elif weapon_name == "SMG":
+                                    ammo = "25, 75"
+                                elif weapon_name == "Cleaner's Carbine":
+                                    ammo = "20, 75"
+                                elif current_slot == "Primary":
+                                    ammo = "1, 25"
+                                else:
+                                    ammo = "N/A"
+                            elif weapon_name != "Widowmaker" and weapon_name != "Short Circuit" and current_slot != "Melee" and len(td_elements) >= 2:  # Ensure there are at least two <td> elements                                                                
+                                if 'rowspan' in td_elements[0].attrs:
+                                    # Reuse the existing loaded_ammo and carried_ammo
+                                    ammo = f"{loaded_ammo}, {carried_ammo}"
+                                else:
+                                    # Extract and store the new ammo values
+                                    loaded_ammo = td_elements[1].get_text(strip=True)
+                                    carried_ammo = td_elements[2].get_text(strip=True)
+                                    ammo = f"{loaded_ammo}, {carried_ammo}"
+
+                                if "N/A" in ammo:
+                                    ammo = "N/A"
+
+                                # Debug print statements (can be removed in production)
+                                #print(f"Weapon: {weapon_name}, Ammo: {ammo}")
+                            else:
+                                ammo = f"{loaded_ammo}, {carried_ammo}"
                             # Insert weapon data into the database
-                            insert_weapon(conn, (weapon_name, current_class, current_slot))
+                            insert_weapon(conn, (weapon_name, current_class, current_slot, ammo))
 
                         
 def remove_duplicates(conn):
@@ -126,7 +184,7 @@ def remove_duplicates(conn):
         # SQL query to remove duplicates
         sql_remove_duplicates = '''
         WITH cte AS (
-            SELECT id, ROW_NUMBER() OVER (PARTITION BY name, class, slot ORDER BY id) AS rn
+            SELECT id, ROW_NUMBER() OVER (PARTITION BY name, class, slot, ammo ORDER BY id) AS rn
             FROM weapons
         )
         DELETE FROM weapons
@@ -146,47 +204,58 @@ def remove_duplicates(conn):
 def print_table(conn):
     try:
         cursor = conn.cursor()
-        cursor.execute("SELECT name, class, slot FROM weapons ORDER BY class")
+        cursor.execute("SELECT name, class, slot, ammo FROM weapons")
 
         rows = cursor.fetchall()
         previous_class = None
+        previous_slot = None
 
         for row in rows:
-            weapon_name, weapon_class, weapon_slot = row
+            weapon_name, weapon_class, weapon_slot, weapon_ammo = row
 
             # Check if the class has changed, print a new line if it has
             if weapon_class != previous_class:
                 if previous_class is not None:
                     print()  # Print a new line when the class changes
                 print(f"Class: {weapon_class}")
+                
+            if previous_slot != weapon_slot and previous_slot != None:
+                print()
+                print(f"Slot: {weapon_slot}")
+                print()
+            elif previous_slot == None and previous_slot != weapon_slot:
+                print(f"Slot: {weapon_slot}")
+                print()
 
             # Print the weapon details
-            print(f"  Slot: {weapon_slot}, Weapon: {weapon_name}")
+            print(f"- {weapon_name}, Ammo: {weapon_ammo}")
             
             # Update the previous_class for the next iteration
             previous_class = weapon_class
+            previous_slot = weapon_slot
 
     except sqlite3.Error as e:
         print(f"Error reading from table: {e}")
         
 def update_duplicate_weapons(conn):
     # Get all weapons from the database
-    weapons = conn.execute("SELECT id, name, class, slot FROM weapons").fetchall()
+    weapons = conn.execute("SELECT id, name, class, slot, ammo FROM weapons").fetchall()
     
-    # Create a dictionary to track weapon counts with slots
+    # Create a dictionary to track weapon counts with slots and ammo
     weapon_count = {}
 
     for weapon in weapons:
         weapon_name = weapon[1]  # Index 1 corresponds to 'name'
         weapon_class = weapon[2]  # Index 2 corresponds to 'class'
         weapon_slot = weapon[3]   # Index 3 corresponds to 'slot'
+        weapon_ammo = weapon[4]   # Index 4 corresponds to 'ammo'
 
         # Create a key for the weapon name
         if weapon_name not in weapon_count:
             weapon_count[weapon_name] = []
 
-        # Append the class and slot as a tuple to the list of classes for this weapon
-        weapon_count[weapon_name].append((weapon_class, weapon_slot))
+        # Append the class, slot, and ammo as a tuple to the list of classes for this weapon
+        weapon_count[weapon_name].append((weapon_class, weapon_slot, weapon_ammo))
 
     # Update weapon names for duplicates
     for weapon_name, class_slot_list in weapon_count.items():
@@ -194,25 +263,28 @@ def update_duplicate_weapons(conn):
             # Build the combined class string
             combined_classes = ", ".join([cls_slot[0] for cls_slot in class_slot_list])
             
-            # Use the slot from the first occurrence
+            # Use the slot and ammo from the first occurrence
             weapon_slot = class_slot_list[0][1]
+            weapon_ammo = class_slot_list[0][2]
 
             # Delete all records with the same name
             conn.execute("DELETE FROM weapons WHERE name = ?", [weapon_name])
-            if weapon_name != "Shotgun" and weapon_name != "B.A.S.E. Jumper":
-                # Insert the single record with combined classes and correct slot
+
+            # Insert the single record with combined classes, correct slot, and ammo
+            if weapon_name not in ["Shotgun", "B.A.S.E. Jumper"]:
                 conn.execute(
-                    "INSERT INTO weapons (name, class, slot) VALUES (?, ?, ?)",
-                    (weapon_name, combined_classes, weapon_slot)
+                    "INSERT INTO weapons (name, class, slot, ammo) VALUES (?, ?, ?, ?)",
+                    (weapon_name, combined_classes, weapon_slot, weapon_ammo)
                 )
             else:
+                # Special handling for Shotgun and B.A.S.E. Jumper
                 conn.execute(
-                "INSERT INTO weapons (name, class, slot) VALUES (?, ?, ?)",
-                (weapon_name, combined_classes, "Primary, Secondary")
+                    "INSERT INTO weapons (name, class, slot, ammo) VALUES (?, ?, ?, ?)",
+                    (weapon_name, combined_classes, "Primary, Secondary", weapon_ammo)
                 )
-                
 
     conn.commit()  # Commit the changes to the database
+
 
 
 
